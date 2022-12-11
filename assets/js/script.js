3b6afc38f78b11ac43bf12524d187d7a9634312e
rubymaxioned <rubym@axioned.com>
2022-12-11 21:42:40 +0530

    added functionlity

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,12 +4,19 @@ var navMenu = document.querySelector('.nav-menu');
 var city = "mumbai";
 var key = "a2dbafd32bfb6d87a7dee017beec62d6";
 
+//navigation
+navToggle.addEventListener('click', function () {
+    hamburger.classList.toggle('open');
+    navMenu.classList.toggle('show');
+})
+
 //Current day
 var day = document.querySelector('.weather-header span:nth-of-type(1)');
 var today = new Date();
 var currentDay = today.getDay();
-var dayString = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-day.innerHTML = dayString[currentDay - 1];
+var dayString = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+day.innerHTML = dayString[currentDay];
+console.log(currentDay);
 
 //Current Date
 var date = document.querySelector('.weather-header span:nth-of-type(2)');
@@ -17,11 +24,6 @@ var todayDate = today.getDate();
 var month = today.getMonth();
 var monthString = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
 date.innerHTML = `${todayDate} ${monthString[month]}`;
-navToggle.addEventListener('click', function () {
-    hamburger.classList.toggle('open');
-    navMenu.classList.toggle('show');
-})
-
 
 //Weather Information
 var searchCity = document.querySelector('.info-container h3');
@@ -38,39 +40,49 @@ button.addEventListener('click', function () {
 function myFunction() {
     var p = fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`);
     p.then(function (response) {
+        console.log(response.status);
+        // if (response.status == 404) {
+        //     console.log(error.message);
+        // }
         return response.json();
     }).then(function (value) {
         console.log(value);
-        searchCity.innerHTML = value.name;
-        var temp = value.main.temp;
-        var currentTemp = temp - 273.15;
-        temperature.innerHTML = currentTemp.toFixed(2) + '&#8451;';
+        if (value.cod == 200) {
+            searchCity.innerHTML = value.name;
+            var temp = value.main.temp;
+            var currentTemp = temp - 273.15;
+            temperature.innerHTML = currentTemp.toFixed(2) + '&#8451;';
 
-        var tempIcon = document.querySelector('.temperature figure img');
-        var icon = value.weather[0].main;
-        if (icon == "Drizzle") {
-            tempIcon.src = "./assets/images/icons/icon-9.svg";
-        } else if (icon == " Thunderstorm") {
-            tempIcon.src = "./assets/images/icons/icon-12.svg";
-        } else if (icon == "Rain") {
-            tempIcon.src = "./assets/images/icons/icon-10.svg";
-        } else if (icon == "Clear") {
-            tempIcon.src = "./assets/images/icons/icon-1.svg";
-        } else if (icon == "Broken clouds") {
-            tempIcon.src = "./assets/images/icons/icon-3.svg";
-        } else if (icon == "Clouds") {
-            tempIcon.src = "./assets/images/icons/icon-5.svg";
-        } else {
-            tempIcon.src = "./assets/images/icons/icon-7.svg";
+            var tempIcon = document.querySelector('.temperature figure img');
+            var icon = value.weather[0].main;
+            if (icon == "Drizzle") {
+                tempIcon.src = "./assets/images/icons/icon-9.svg";
+            } else if (icon == " Thunderstorm") {
+                tempIcon.src = "./assets/images/icons/icon-12.svg";
+            } else if (icon == "Rain") {
+                tempIcon.src = "./assets/images/icons/icon-10.svg";
+            } else if (icon == "Clear") {
+                tempIcon.src = "./assets/images/icons/icon-1.svg";
+            } else if (icon == "Broken clouds") {
+                tempIcon.src = "./assets/images/icons/icon-3.svg";
+            } else if (icon == "Clouds") {
+                tempIcon.src = "./assets/images/icons/icon-5.svg";
+            } else {
+                tempIcon.src = "./assets/images/icons/icon-7.svg";
+            }
+
+            var humidity = document.querySelector('.weather-info li:nth-of-type(1) span');
+            var speed = document.querySelector('.weather-info li:nth-of-type(2) span');
+            var direction = document.querySelector('.weather-info li:nth-of-type(3) span');
+            humidity.innerHTML = value.main.humidity + "%";
+            speed.innerHTML = value.wind.speed + "m/sec";
+            direction.innerHTML = value.wind.deg;
+            console.log(direction);
+        }
+        if (value.cod == 404) {
+            searchCity.innerHTML = "Searched City Not Found";
         }
 
-        var humidity = document.querySelector('.weather-info li:nth-of-type(1) span');
-        var speed = document.querySelector('.weather-info li:nth-of-type(2) span');
-        var direction = document.querySelector('.weather-info li:nth-of-type(3) span');
-        humidity.innerHTML = value.main.humidity + "%";
-        speed.innerHTML = value.wind.speed + "m/sec";
-        direction.innerHTML = value.wind.deg;
-        console.log(direction);
     })
 }
 myFunction();
